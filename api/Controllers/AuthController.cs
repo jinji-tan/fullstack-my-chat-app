beb0697b31b0562c5d4d43fcceda282b6f9cefcf
Louis Tan <louistan600@gmail.com>
2026-03-27 00:52:06 +0800

    setup auth controller

---
+++ api/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+using System.Security.Cryptography;
+using System.Text;
+using api.DTOs;
+using api.Repositories.interfaces;
+using api.Service.interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ITokenService _token;
+        private readonly IAuthHelper _auth;
+        public AuthController(ITokenService token, IAuthHelper auth)
+        {
+            _token = token;
+            _auth = auth;
+        }
+
+        [HttpPost("/api/register")]
+        public async Task<IActionResult> Register(UserDto userDto)
+        {
+            if (await _auth.UserExists(userDto.Email))
+                return BadRequest("Email already exists.");
+
+            var user = await _auth.Register(userDto);
+
+            if (user == false)
+                return BadRequest("Failed to register.");
+
+            return Ok("Successfully registered.");
+        }
+
+
+        [HttpPost("/api/login")]
+        public async Task<IActionResult> Login(AuthDto authDto)
+        {
+            var user = await _auth.GetUserByEmail(authDto.Email);
+
+            if (user == null)
+                return Unauthorized("Invalid email or password.");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            byte[] computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(authDto.Password));
+
+            if (!computedHash.SequenceEqual(user.PasswordHash))
+                return Unauthorized("Invalid email or password.");
+
+            return Ok(new { token = _token.CreateToken(user.Email) });
+        }
+    }
+}
