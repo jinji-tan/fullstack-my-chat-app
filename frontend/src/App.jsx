c7ce7a4652fc289ccec0430c01177e5bab5c6f13
Louis Tan <louistan600@gmail.com>
2026-03-27 04:10:31 +0800

    setup renderPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,42 @@
-
+import { useEffect, useState } from "react"
+import Login from "./components/Login"
+import Register from "./components/Register"
+import Home from "./components/Home"
 
 const App = () => {
-  return <h1>Hola mundo</h1>
+  const [page, setPage] = useState(() => {
+    const savedToken = localStorage.getItem("token");
+    const savedPage = localStorage.getItem("page");
+
+    if (savedToken) return "home"
+    return savedPage || "login"
+  });
+
+  useEffect(() => {
+    localStorage.setItem("page", page)
+
+    const token = localStorage.getItem("token");
+    if (page === "home" && !token) {
+      setPage("login");
+    }
+
+  }, [page])
+
+  const renderPage = () => {
+    switch (page) {
+      case "login":
+        return <Login setPage={setPage} />
+      case "register":
+        return <Register setPage={setPage} />
+      case "home":
+        return <Home setPage={setPage} />
+      default:
+        return <Login setPage={setPage} />
+    }
+  }
+
+  return <>{renderPage()}</>
+
 }
 
 export default App
